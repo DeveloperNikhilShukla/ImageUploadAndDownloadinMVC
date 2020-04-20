0b04c99af83f914dce12a3f3d0fb958afc38c984
The Epic Hero <63442799+TheIndianHacker-crypto@users.noreply.github.com>
2020-04-21 00:42:22 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImageUploadAndDownloadInMVC.Models;
+namespace ImageUploadAndDownloadInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FileUploaderUpdate(Employee _Employee)
+        {
+            string path = Server.MapPath("~/uploadfile");
+            string filename =Path.GetFileName(_Employee.file.FileName);
+            string FullPath = Path.Combine(path, filename);
+            _Employee.file.SaveAs(FullPath);
+
+
+            return View("index");
+        }
+
+        public FileResult Download(Employee _Employee)
+        {
+            string path = Server.MapPath("~/uploadfile");
+            string filename = Path.GetFileName("excel.html");
+            string FullPath = Path.Combine(path, filename);
+           // _Employee.file.SaveAs(FullPath);
+            return File(FullPath, "text/html","DownloadData.html");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImageUploadAndDownloadInMVC.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public HttpPostedFileBase file{get;set;}
+    }
+}
